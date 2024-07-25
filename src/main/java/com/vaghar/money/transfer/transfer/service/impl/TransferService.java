1f0ccefbc83f3e054b94f67a7c1218199bd5d8cf
danial <danialvaghar@gmail.com>
2024-07-25 13:02:03 +0330

    check source and destination account equivalence

--- src/main/java/com/vaghar/money/transfer/transfer/service/impl/TransferService.java
+++ src/main/java/com/vaghar/money/transfer/transfer/service/impl/TransferService.java
@@ -4,8 +4,10 @@ import com.vaghar.money.transfer.account.service.IAccountService;
 import com.vaghar.money.transfer.transfer.model.dto.TransferDto;
 import com.vaghar.money.transfer.transfer.service.ITransferService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +18,9 @@ public class TransferService implements ITransferService {
     @Override
     @Transactional
     public void transferMoney(TransferDto dto) {
+        if (dto.getSourceAccount().equals(dto.getDestinationAccount())) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "source and destination account cannot be the same");
+        }
         accountService.updateBalance(dto.getSourceAccount(), dto.getAmount().negate());
         accountService.updateBalance(dto.getDestinationAccount(), dto.getAmount());
     }
